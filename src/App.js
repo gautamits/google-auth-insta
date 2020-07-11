e6809eb87d6db5f4f7a8b3066cf58bf4628d44ba
amit <gautamamits95@gmail.com>
2020-07-11 15:42:48 +0530

    login working on localhost

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css';import { GoogleLogin } from 'react-google-login';
 
 function App() {
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin
+        clientId="753999909949-dag78tucj9or3v1aci69jghdshrt5d9c.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
     </div>
   );
 }
 
 export default App;
+
+//client secret pO_iAfHJZ_fpkFlVrzU_QCDh
