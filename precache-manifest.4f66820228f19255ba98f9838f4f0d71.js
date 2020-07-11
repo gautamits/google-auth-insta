721dabe66d2dc92e09ce123cea30cdee443d8b78
amit <gautamamits95@gmail.com>
2020-07-11 15:45:28 +0530

    Updates

---
+++ precache-manifest.4f66820228f19255ba98f9838f4f0d71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab8627453f34623bf1c47216a4e31e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c3c48be44d2b18284f8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2ec8af12a62e41088d0",
+    "url": "/static/js/2.14b7a32e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.14b7a32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3c48be44d2b18284f8",
+    "url": "/static/js/main.4b42bf22.chunk.js"
+  },
+  {
+    "revision": "9a14e40db1e70295ff1e",
+    "url": "/static/js/runtime-main.0728639d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
